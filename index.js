69c7682e4635fa2215db02e282309f244c21913a
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-27 18:49:58 +0600

    Customised the json data in the server & Enable to show them on browser

--- index.js
+++ index.js
@@ -7,13 +7,37 @@ const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
 
+const courses = require('./data/courses.json');
+
+
+
 app.get('/', (req, res)=>{
     res.send('Tutor API running');
 });
 
 app.get('/course-categories', (req, res) =>{
-    res.send(categories)
+    res.send(categories);
 })
+
+app.get('/course/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.course_id === id );
+     res.send(selectedCourse);
+} )
+
+
+app.get('/course-category/:id', (req,res)=>{
+    const id = req.params.id;
+    if(id === "7"){
+        res.send(courses);
+    }
+    else{
+        const course_category = courses.filter(course => course.category_id === id);
+        res.send(course_category); 
+    }
+       
+})
+
 
 
 app.listen(port, ()=>{
